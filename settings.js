da4b75c2a69d1d424141cd88e53a2311cb60cf7e
Georges62 <georgeszeutsa@gmail.com>
2025-06-15 22:27:30 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const settings = {
   packname: 'Knight Bot',
   author: '‎',
   botName: "Knight Bot",
-  botOwner: 'knigt bot', // Your name
+  botOwner: 'Knigt Bot', // Your name
   ownerNumber: '+237658131143', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
